ce3b55431f32f8ddc40aba48e35b1a66974f477f
hafiz <hafizur.pilot@gmail.com>
2020-11-30 23:26:14 -0500

    javaClassAddByHafiz

---
+++ Mohammed-Group/src/main/java/mohammed/group/javaClassHafiz.java
@@ -0,0 +1,13 @@
+package mohammed.group;
+
+public class javaClassHafiz {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println(" This is practice for git  ");
+		
+	}            
+
+}
